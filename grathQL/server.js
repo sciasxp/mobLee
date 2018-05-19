84d4368f05150e990c71a00dd6e646b8f24e2e76
Luciano Bastos Nunes <sciasxp@gmail.com>
2018-05-19 12:17:34 -0300

    Add CORS dependence to graphQL + Update node

--- grathQL/server.js
+++ grathQL/server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const expressGraphQL = require('express-graphql');
 const schema = require('./schema.js');
 
+const cors = require('cors')
 const app = express();
+
+app.use(cors())
 
 app.use('/graphql', expressGraphQL({
 	schema: schema,
